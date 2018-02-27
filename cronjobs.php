9c1f63392572cc80a145138b21bc9dbf11fb5c2f
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-02-28 00:37:50 +0700

    Style fix

--- cronjobs.php
+++ cronjobs.php
@@ -1,26 +1,26 @@
 <?php
 include_once 'autoload.php';
 
-$user = new User();
-$userdata = $user->lostUpate();
+$user 			= new User();
 
-$access_token = $userdata['access_token'];
-$user_id = $userdata['id'];
-$user_name = $userdata['name'];
+$userdata 		= $user->lostUpate();
+
+$access_token 	= $userdata['access_token'];
+$user_id 		= $userdata['id'];
+$user_name 		= $userdata['name'];
 
 if(empty($user_id) || empty($access_token)){
 	echo 'Access token <strong>is empty!</strong>';
 	exit(0);
 }
 
-$WakaTime = new WakaTime();
+$WakaTime 		= new WakaTime();
 
-$end	= date('Y-m-d',strtotime("-1 days"));
-$start 	= date('Y-m-d',strtotime("-3 days"));
+$end			= date('Y-m-d',strtotime("-1 days"));
+$start 			= date('Y-m-d',strtotime("-3 days"));
+$summaries 		= $WakaTime->summaries($start,$end,$access_token);
 
-$summaries = $WakaTime->summaries($start,$end,$access_token);
-
-$activity = new Activity();
+$activity 		= new Activity();
 
 foreach ($summaries->data as $data){
 	$date = $data->range->date;
