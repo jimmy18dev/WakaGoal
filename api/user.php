55d5be7db939386d2c76dfab823ef35e4fa29f8f
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-04-18 15:20:46 +0700

    Access Control

--- api/user.php
+++ api/user.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../autoload.php';
 header("Content-type: application/json");
-// header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 
 $returnObject = array(
 	"apiVersion"  	=> 1.0,
