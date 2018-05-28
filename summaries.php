4fbae358235bca4b677754f4a4b0ee2f7df4cb2a
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-05-28 10:00:48 +0700

    Fix summaries bug

--- summaries.php
+++ summaries.php
@@ -2,31 +2,35 @@
 include_once 'autoload.php';
 
 $WakaTime = new WakaTime();
+$activity = new Activity();
 
 $today 	= date('Y-m-d',strtotime("-1 days"));
 $sixday = date('Y-m-d',strtotime("-6 days"));
 
 $summaries = $WakaTime->summaries($sixday,$today,$user->access_token);
 
-$activity = new Activity();
+print_r($summaries);
 
-foreach ($summaries->data as $data){
-	$date = $data->range->date;
+if(count($summaries) > 0){
+	foreach ($summaries->data as $data){
 
-	// Languages activity
-	foreach ($data->languages as $var){
-		$language 		= $var->name;
-		$total_seconds 	= $var->total_seconds;
+		$date = $data->range->date;
 
-		$activity->add($user->id,$language,$total_seconds,$date);
-	}
+		// Languages activity
+		foreach ($data->languages as $var){
+			$language 		= $var->name;
+			$total_seconds 	= $var->total_seconds;
 
-	// Projects activity
-	foreach ($data->projects as $var) {
-		$name 			= $var->name;
-		$total_seconds 	= $var->total_seconds;
+			$activity->add($user->id,$language,$total_seconds,$date);
+		}
 
-		$activity->addProject($user_id,$name,$total_seconds,$date);
+		// Projects activity
+		foreach ($data->projects as $var) {
+			$name 			= $var->name;
+			$total_seconds 	= $var->total_seconds;
+
+			$activity->addProject($user->id,$name,$total_seconds,$date);
+		}
 	}
 }
 
